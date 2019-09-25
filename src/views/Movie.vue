468f5cf8a28b291febc0500796f4ccf8eb7b3f68
y31 <525998520@qq.com>
2019-09-26 04:15:57 +0800

    style

--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -1,48 +1,74 @@
 <template>
   <div>
-    <router-link to="/">click</router-link>
-    <h2>Movie</h2>
-    <div class="header">
-        <img :src="movie.images.small">
-        <div class="msgBox">
-        <p class="director">导演：<span v-for="director of movie.directors" :key="director.id">{{ director.name }}/</span></p>
-        <p class="performer">主演：<span v-for="performer of movie.casts" :key="performer.id">{{ performer.name }}/</span></p>
-        <p>类型：<span v-for="(genre,index) of movie.genres" :key="index"> {{ genre }} </span></p>
-        <p>制片国家/地区：{{ movie.countries[0] }}</p>
-        <p>语言：{{ movie.languages[0] }}</p>
-        <p>上映时间：{{ movie.pubdates[0] }}</p>
-        <p>片长：{{ movie.durations[0] }}</p>
-        <div>123</div>
+    <el-container>
+      <el-header>
+        <h2>Movie</h2>
+        <router-link to="/">Back</router-link>
+      </el-header>
+      <el-main>
+        <div class="header">
+          <div class="clearFix">
+            <img :src="movie.images.small" />
+          </div>
+          <div class="msgBox">
+            <p>
+              类型：
+              <span v-for="(genre,index) of movie.genres" :key="index">{{ genre }}</span>
+            </p>
+            <p>制片国家/地区：{{ movie.countries[0] }}</p>
+            <p>语言：{{ movie.languages[0] }}</p>
+            <p>上映时间：{{ movie.pubdates[0]}}</p>
+            <p>片长：{{ movie.durations[0] }}</p>
+          </div>
+          <div class="person">
+            <p class="director">
+              导演：
+              <span v-for="director of movie.directors" :key="director.id">{{ director.name }}/</span>
+            </p>
+            <el-divider></el-divider>
+            <p class="performer">
+              主演：
+              <span v-for="performer of movie.casts" :key="performer.id">{{ performer.name }}/</span>
+            </p>
+            <el-divider></el-divider>
+          </div>
         </div>
-    </div>
-    <div class="reviews">
-        <div v-for="review of movie.popular_reviews" :key="review.id">
-            <div>《{{review.title}}》</div>
-            <div>{{review.summary}}</div>
-            <br>
-        </div>
-    </div>
+      </el-main>
+      <el-main>
+      <h3>Popular-review</h3>
+      <el-collapse>
+        <el-collapse-item
+          :title="review.title"
+          :name="review.id"
+          v-for="review of movie.popular_reviews"
+          :key="review.id"
+        >
+          <div class="review">{{review.summary}}</div>
+        </el-collapse-item>
+      </el-collapse>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
-import { mapMutations, mapState,mapGetters,mapActions } from "vuex";
+import { mapMutations, mapState, mapGetters, mapActions } from "vuex";
 import { format } from "path";
 export default {
   name: "Movie",
   data() {
-      return {
-          testData: {
-              name:'lila',
-              age:12
-          }
+    return {
+      testData: {
+        name: "lila",
+        age: 12
       }
+    };
   },
   computed: {
-    ...mapGetters([ 'movie' ])
+    ...mapGetters(["movie"])
   },
   methods: {
-    ...mapActions(['getMovieMsg'])
+    ...mapActions(["getMovieMsg"])
   },
   created() {
     this.getMovieMsg(this.$route.query.id);
@@ -50,24 +76,66 @@ export default {
 };
 </script>
 
-<style lang='stylus' scope>
-.header
-    // text-align left 
-    margin 0 4vw
-    img
-        // margin-left 2vw
-        // float left
-        width 40%
-        display inline-block
-    div
-        display block
-.msgBox 
-    position relative
-    // right -8vw
-.director
-.performer
-    text-indent -2em
-    margin-left 2em
+<style lang='stylus' scoped>
+.header {
+  margin: 0 4vw;
 
+  img {
+    float: left;
+    width: 50%;
+    display: inline-block;
+    position: relative;
+  }
 
+  div {
+    display: block;
+  }
+}
+
+.msgBox {
+  position: absolute;
+  left: 56vw;
+  top: 21vw;
+}
+
+.director, .performer {
+  text-indent: -2em;
+  margin-left: 2em;
+}
+
+.review {
+  text-indent: 2rem;
+}
+
+.clearFix::after {
+  display: block;
+  content: '';
+  height: 0;
+  visibility: hidden;
+  clear: both;
+}
+
+.person {
+  margin-top: 10vw;
+}
+
+.el-main:first-of-type {
+  padding-top 8vw
+  padding-bottom: 0;
+}
+
+.el-divider {
+  margin: 3vw 0;
+}
+
+h2 {
+  display inline-block  
+}
+a {
+  font-size 1.3em
+  float: right;
+  position relative
+  top 50%
+  transform translateY(-50%)
+}
 </style>
